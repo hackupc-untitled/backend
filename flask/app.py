08b073967b7c0e022fca2f8159d46006e52b76c6
Marc <marc.martin1999@gmail.com>
2024-05-04 11:32:23 +0200

    Creados endpoints y clase para conectarse

--- flask/app.py
+++ flask/app.py
@@ -4,6 +4,7 @@ from clients import *
 #from mi_app.posts.views import posts_bp
 
 app = Flask(__name__)
+client = ''
 
 @app.route('/save', methods=['POST'])
 def save():
@@ -12,16 +13,21 @@ def save():
     client.write_points(json_data)
 
 
-#@app.route('/', methods=['GET'])
-
-
-@app.route('/test', methods=['GET'])
-def test():
+#Each app.route represents an endpoint
+@app.route('/countGalileos', methods=['GET'])
+def countGalileos(mac_address):
+    query = 'SELECT * FROM raw WHERE mac_address = \'' + mac_address + '\' LIMIT 10'
+    result = client.query(query)
     return Response(status=200)
-    
-    
-    
 
+
+
+
+@app.route('/test', methods=['POST'])
+def test():
+    print(request.get_json())
+    return Response(status=200)
+        
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000,debug=True)  # Change port number if needed
